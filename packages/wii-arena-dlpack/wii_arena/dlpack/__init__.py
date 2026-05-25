d82022b096e3e08db3001b71602b4647690f1db9
Minjae Gwon <beta.underalpha@gmail.com>
2026-05-25 21:40:37 +0000

    feat: define protocol for dlpack

---
+++ packages/wii-arena-dlpack/wii_arena/dlpack/__init__.py
@@ -0,0 +1,33 @@
+from enum import IntEnum
+from types import CapsuleType
+from typing import Protocol, TypeAlias
+
+
+class DlpackDeviceType(IntEnum):
+    CPU = 1
+    CUDA = 2
+    CPU_PINNED = 3
+    OPENCL = 4
+    VULKAN = 7
+    METAL = 8
+    VPI = 9
+    ROCM = 10
+    CUDA_MANAGED = 13
+    ONE_API = 14
+
+
+DlpackDevice: TypeAlias = tuple[DlpackDeviceType, int]
+DlpackVersion: TypeAlias = tuple[int, int]
+
+
+class SupportsDlpack(Protocol):
+    def __dlpack__(
+        self,
+        *,
+        stream: object | None = None,
+        max_version: DlpackVersion | None = None,
+        dl_device: DlpackDevice | None = None,
+        copy: bool | None = None,
+    ) -> CapsuleType: ...
+
+    def __dlpack_device__(self) -> DlpackDevice: ...
