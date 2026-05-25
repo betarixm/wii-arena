2956440c1b412ee6e003f62159b48fa5b1d2e66c
Minjae Gwon <beta.underalpha@gmail.com>
2026-05-25 22:20:35 +0000

    feat: fix the protocol of `DolphinEnvironment`

--- packages/wii-arena-dolphin/wii_arena/dolphin/__init__.py
+++ packages/wii-arena-dolphin/wii_arena/dolphin/__init__.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
-from types import CapsuleType
-from typing import NewType
+from types import CapsuleType, TracebackType
+from typing import NewType, Self
 
 from wii_arena.core.environment.protocols import Environment
+from wii_arena.core.environment.types import Terminated, Truncated
 from wii_arena.dlpack import (
     DlpackDevice,
     DlpackVersion,
@@ -29,7 +30,34 @@ class DolphinFrameBuffer(SupportsDlpack, ABC):
 
 class DolphinEnvironment[Action](
     Environment[tuple[DolphinMemoryView, DolphinFrameBuffer], Action, None], ABC
-): ...
+):
+    def __init__(self, dolphin: Dolphin) -> None:
+        self._dolphin = dolphin
+
+    def reset(self) -> tuple[tuple[DolphinMemoryView, DolphinFrameBuffer], None]:
+        return (self._dolphin.view_memory(), self._dolphin.view_frame_buffer()), None
+
+    def step(
+        self, action: Action
+    ) -> tuple[
+        tuple[DolphinMemoryView, DolphinFrameBuffer], Terminated, Truncated, None
+    ]:
+        self._execute_action(action)
+        return (
+            (self._dolphin.view_memory(), self._dolphin.view_frame_buffer()),
+            Terminated(self._terminated()),
+            Truncated(self._truncated()),
+            None,
+        )
+
+    @abstractmethod
+    def _execute_action(self, action: Action) -> None: ...
+
+    @abstractmethod
+    def _terminated(self) -> bool: ...
+
+    @abstractmethod
+    def _truncated(self) -> bool: ...
 
 
 class Dolphin(ABC):
@@ -37,3 +65,12 @@ class Dolphin(ABC):
     def view_memory(self) -> DolphinMemoryView: ...
     @abstractmethod
     def view_frame_buffer(self) -> DolphinFrameBuffer: ...
+    @abstractmethod
+    def __enter__(self) -> Self: ...
+    @abstractmethod
+    def __exit__(
+        self,
+        exc_type: type[BaseException],
+        exc: BaseException | None,
+        tb: TracebackType | None,
+    ): ...
